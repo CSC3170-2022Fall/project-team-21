ee6edd7f7e186ac2e0a3996103092e716c32fbf8
lianjiawei <119030043@link.cuhk.edu.cn>
2022-12-27 19:59:11 +0800

    row_test::check_size() finished

--- testing/unit_test/Row_test.h
+++ testing/unit_test/Row_test.h
@@ -11,10 +11,37 @@
 namespace row_test
 {
     std::vector<std::string> row_value = {"this", "is", "a", "test", "for", "row"};
+    Row row = Row(row_value);
 
-    void row_test() {
-        Row row = Row(row_value);
+    void check_size(Row row, std::vector<std::string> value)
+    {
+        auto row_size = row.getValues().size();
+        auto value_size = value.size();
+
+        std::cout << "The row size is " << row_size << std::endl;
+        std::cout << "The test value size is " << value_size << std::endl;
+
+        EXPECT_EQ(row_size, value_size);
     }
 
+    void check_value()
+    {
+        
+
+    }
+
+    void check_setValue()
+    {
+    }
+
+    void row_test()
+    {
+
+        std::cout << "[===============================================================]" << std::endl;
+        std::cout << "[----------------- Run container test : deque ------------------]" << std::endl;
+
+        check_size(row, row_value);
+        PASSED;
+    }
 
 }
