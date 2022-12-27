787713accb49fe0cb4356966843967d59306a242
lianjiawei <119030043@link.cuhk.edu.cn>
2022-12-27 20:14:18 +0800

    test.h: EXPECT_CON_EQ

--- testing/unit_test/test.h
+++ testing/unit_test/test.h
@@ -15,6 +15,20 @@
     std::cout << red << " Actual:" << v2 << "\n";               \
 }} while(0)
 
+#define EXPECT_CON_EQ(c1, c2) do {                                  \
+  auto first1 = std::begin(c1), last1 = std::end(c1);               \
+  auto first2 = std::begin(c2), last2 = std::end(c2);               \
+  for (; first1 != last1 && first2 != last2; ++first1, ++first2) {  \
+    if (*first1 != *first2)  break;                                 \
+  }                                                                 \
+  if (first1 == last1 && first2 == last2) {                         \
+    std::cout << green << " EXPECT_CON_EQ succeeded!\n";            \
+  }                                                                 \
+  else {                                                            \
+    std::cout << red << " EXPECT_CON_EQ failed!\n";                 \
+    std::cout << red << " Expect:" << *first1 << "\n";              \
+    std::cout << red << " Actual:" << *first2 << "\n";              \
+}} while(0)
 
 
 
