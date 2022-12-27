7cc73792001be3217e9d932dd07c50ba21ef150e
lianjiawei <119030043@link.cuhk.edu.cn>
2022-12-27 20:15:15 +0800

    row_test::check_value

--- testing/unit_test/Row_test.h
+++ testing/unit_test/Row_test.h
@@ -18,15 +18,20 @@ namespace row_test
         auto row_size = row.getValues().size();
         auto value_size = value.size();
 
-        std::cout << "The row size is " << row_size << std::endl;
-        std::cout << "The test value size is " << value_size << std::endl;
+        std::cout << " The row size is " << row_size << std::endl;
+        std::cout << " The test value size is " << value_size << std::endl;
 
         EXPECT_EQ(row_size, value_size);
     }
 
-    void check_value()
+    void check_value(Row row, std::vector<std::string> actual_value)
     {
-        
+        auto row_value = row.getValues();
+
+        COUT(row_value);
+        COUT(actual_value);
+
+        EXPECT_CON_EQ(row_value, actual_value);
 
     }
 
@@ -40,7 +45,15 @@ namespace row_test
         std::cout << "[===============================================================]" << std::endl;
         std::cout << "[----------------- Run container test : deque ------------------]" << std::endl;
 
+        std::cout << "Check row size" << std::endl;
         check_size(row, row_value);
+
+        std::cout << "Check row values" << std::endl;
+        check_value(row, row_value);
+
+        std::cout << "Check Row::setValues" << std::endl;
+        check_setValue();
+
         PASSED;
     }
 
